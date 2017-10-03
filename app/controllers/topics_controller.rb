fe26d492ee8601c44dda4f0b0f9a2c54b88e733e
Muhammad <daudjaved1@gmail.com>
2017-10-03 01:52:39 +0000

    completed topics crud

---
+++ app/controllers/topics_controller.rb
@@ -0,0 +1,63 @@
+class TopicsController < ApplicationController
+
+  def index
+    @topics = Topic.all
+  end
+
+  def show
+    @topic = Topic.find(params[:id])
+  end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new(topic_params)
+
+    if @topic.save
+      flash[:notice] = "Topic was saved successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error creating the topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
+
+  def update
+    @topic = Topic.find(params[:id])
+
+    @topic.assign_attributes(topic_params)
+
+    if @topic.save
+      flash[:notice] = "Topic was updated"
+      redirect_to @topic
+    else
+      flash[:error] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:id])
+
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.title}\" was deleted successfully."
+      redirect_to topics_path
+    else
+      flash[:error] = "There was an error deleting the topic."
+      render :show
+    end
+  end
+
+  private
+
+  def topic_params
+    params.require(:topic).permit(:title, :user)
+  end
+
+end
