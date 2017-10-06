a531e10084712b92924b307ff290b6945e884fea
Muhammad <daudjaved1@gmail.com>
2017-10-06 15:54:18 +0000

    completed incoming controller 2

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -3,16 +3,11 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    puts "INCOMING PARAMS HERE: #{params}"
     @user =  User.find_by(email: params["sender"])
     @topic = Topic.find_by(title: params["subject"])
-    p @user
-    p @topic
-    p params["body-plain"]
     @url = params["body-plain"]
 
     if @user.nil?
-      puts "created user"
       @user = User.create(email: params["sender"], password: '1234567')
     end
 
@@ -20,7 +15,7 @@ class IncomingController < ApplicationController
       @topic = Topic.create(title: params["subject"], user: @user)
     end
 
-    @bookmark = @topic.bookmarks.create(url: @url)
+    @bookmark = @topic.bookmarks.create(url: @url, user: @user)
 
     head 200
    
