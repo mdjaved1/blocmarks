28c08be682e5945915bfa8bf71167f4e6ae13741
Muhammad <daudjaved1@gmail.com>
2017-10-06 15:32:18 +0000

    testing incoming

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -4,17 +4,17 @@ class IncomingController < ApplicationController
 
   def create
     puts "INCOMING PARAMS HERE: #{params}"
-    @user =  User.find_by(email: params[:sender])
-    @topic = Topic.find_by(title: params[:subject])
+    @user =  User.find_by(email: params["sender"])
+    @topic = Topic.find_by(title: params["subject"])
 
     @url = params["body-plain"]
 
     if @user.nil?
-      @user = User.create(email: params[:sender], password: '123')
+      @user = User.create(email: params["sender"], password: '123')
     end
 
     if @topic.nil?
-      @topic = Topic.create(title: params[:subject], user_id: @user)
+      @topic = Topic.create(title: params["subject"], user: @user)
     end
 
     @bookmark = @topic.bookmarks.create(url: @url)
